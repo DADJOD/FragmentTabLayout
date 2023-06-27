b6d26147a7e0fbec744b68907c03eebb107281f7
DADJOD <kamu57@mail.ru>
2023-06-27 15:18:16 +0300

    add FragmentStateAdapter and delete deprecated classes

--- app/src/main/java/com/example/fragmenttablayout/MainActivity.kt
+++ app/src/main/java/com/example/fragmenttablayout/MainActivity.kt
@@ -6,32 +6,34 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
-import androidx.viewpager.widget.ViewPager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 
-@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     // Виджеты
     private var toolbar: Toolbar? = null
     private var tabLayout: TabLayout? = null
-    private var viewPager: ViewPager? = null
+    private var viewPager: ViewPager2? = null
     private var adapter: ViewPagerAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         toolbar = findViewById<View>(R.id.toolbar) as Toolbar
         tabLayout = findViewById<View>(R.id.tabs) as TabLayout
-        viewPager = findViewById<View>(R.id.viewpager) as ViewPager
+        viewPager = findViewById<View>(R.id.viewpager) as ViewPager2
 
         // Устанавливаем Toolbar в качестве ActionBar
         // ActionBar удаляется с помощью темы в styles.xml
         setSupportActionBar(toolbar)
         supportActionBar?.title = "Tab activity"
-        tabLayout?.setupWithViewPager(viewPager)
+//        tabLayout?.setupWithViewPager(viewPager)
 
-        adapter = ViewPagerAdapter(supportFragmentManager)
+        adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
 
         adapter?.addFragment(
             GenericFragment.newInstance(R.color.colorAccent, "First"), "First"
@@ -54,29 +56,38 @@ class MainActivity : AppCompatActivity() {
         )
 
         viewPager?.adapter = adapter
+
+        TabLayoutMediator(tabLayout!!, viewPager!!) { tab, pos ->
+            tab.text = adapter?.getPageTitle(pos)
+        }.attach()
     }
 
-    // Цвета - целые числа
-    // 0xaaccbbaa 0x - шестнадцатиричный ARGB Alpha Red Green Blue 00-ff
-    // 0xccbbaa - без Alpha - полностью непрозрачный
-    internal class ViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    internal class ViewPagerAdapter(fm: FragmentManager, lifecycle: Lifecycle) :
+        FragmentStateAdapter(fm, lifecycle) {
         // Контейнеры для фрагментов и заголовков табов
         private val titles: MutableList<String> = mutableListOf()
         private val fragments: MutableList<Fragment> = mutableListOf()
 
-        override fun getItem(position: Int): Fragment {
-            return fragments[position]
-        }
+//        override fun getItem(position: Int): Fragment {
+//            return fragments[position]
+//        }
 
-        override fun getCount(): Int {
+//        override fun getCount(): Int {
+//            return fragments.size
+//        }
+
+        override fun getItemCount(): Int {
             return fragments.size
         }
 
-        override fun getPageTitle(position: Int): CharSequence {
+        override fun createFragment(position: Int): Fragment {
+            return fragments[position]
+        }
+
+        fun getPageTitle(position: Int): CharSequence {
             return titles[position]
         }
 
-        // Добавление фрагментов во ViewPager
         fun addFragment(fragment: Fragment, title: String) {
             fragments.add(fragment)
             titles.add(title)
